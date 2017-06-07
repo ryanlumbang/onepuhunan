72428a3649e461c7785d61c9392e51c25de27949
ryan.lumbang <ryan.lumbang@onepuhunan.com.ph>
2017-06-07 17:19:26 +0800

    BUGS UPDATE

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,10 +1,7 @@
 <?php
 if($this->session->role_id == 'sa'  OR $this->session->role_id == 'super' OR $this->session->role_id == 'tc'
 OR $this->session->role_id == 'qa'  OR $this->session->role_id == 'bm' OR $this->session->role_id == 'cpu'
-OR $this->session->role_id == 'ssuper'
-
-
-){?>
+OR $this->session->role_id == 'ssuper'){?>
 
 <?php
     $data['title'] = 'OnePuhunan Service Portal | Dashboard';
