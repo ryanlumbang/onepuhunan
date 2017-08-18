52cb8ddb53495fa4587d61d79b2f0388d023d333
ajei.najera <ajei.najera@onepuhunan.com.ph>
2017-08-18 16:29:30 +0800

    update

--- application/views/validation/success_branch_assign.php
+++ application/views/validation/success_branch_assign.php
@@ -1,23 +1,20 @@
 <?php
 $data['title'] = 'OnePuhunan Service Portal | Registration Successful';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<?php $this->load->view("templates/head", $data); ?>
-<body>
-<?php $this->load->view("templates/header"); ?>
-<div class="page-wrap">
-    <div id="tm-container" class="uk-container uk-width-5-10 uk-container-center">
-        <?=form_open("", array("class" => "uk-form uk-form-horizontal"));?>
-        <legend class="uk-text-muted uk-text-bold uk-text-success tm-form-legend">
-            <i class="uk-icon-check-circle uk-text-success"></i> BRANCH ASSIGN SUCCESSFUL!
-        </legend>
-        <div class="uk-form-row uk-text-center uk-margin-large-top">
-            <a class="uk-button uk-button-primary uk-button-small uk-width-3-10" href="<?php echo site_url("dashboard"); ?>">GO TO DASHBOARD PAGE</a>
+<?php $this->load->view("onepuhunan/header",$data); ?>
+<div class="main">
+    <div class="main-inner">
+        <div class="container">
+            <?=form_open("", array("class" => "form-width-small text-center"));?>
+            <h2 class="text-success">
+                <i class="glyphicon glyphicon-ok"></i> BRANCH ASSIGN SUCCESSFUL!
+            </h2>
+            <div class="form-group default-margin">
+                <a class="btn global-button" href="<?php echo site_url("dashboard"); ?>">GO TO DASHBOARD PAGE</a>
+            </div>
+            <?=form_close();?>
         </div>
-        <?=form_close();?>
     </div>
 </div>
-<?php $this->load->view("templates/footer"); ?>
-</body>
-</html>
+<?php $this->load->view("onepuhunan/copyright"); ?>
+<?php $this->load->view("onepuhunan/footer"); ?>
