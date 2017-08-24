73680f6791255fe3137df19631070e245b888b60
vill.labrador <vill.labrador@onepuhunan.com.ph>
2017-08-24 15:42:48 +0800

    workaround

--- application/views/operations/los_info.php
+++ application/views/operations/los_info.php
@@ -16,25 +16,18 @@ $err = '&nbsp;&nbsp;<div class="uk-badge uk-badge-danger" data-uk-tooltip="{pos:
                         <h4 class="badge"><?=$cl_tags['DestProcess']?></h4>
                     </div>
                 </div>
-                <!-- error messages -->
-                <?php if (count($cl_error) > 0) { ?>
-                    <div class='alert alert-warning'><b>This account contains the following error(s):</b>
-                        <ul style="margin-top: 1px">
-                            <?php
-                            foreach((array) $cl_error as $row) {
-                                $error = '<li>' . $row['ErrorDesc'] . '</li>';
-                                echo $error;
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                <?php } ?>
 
-                <!-- client's snapshot -->
-                <?php if (count($cl_error) > 0) { ?>
+                <div id="get_los_laf_err">
+                    <!-- error messages -->
 
-                <?php } else { ?>
-                        <?php } ?>
+                        <div class='alert alert-warning'>
+                            <ul style="margin-top: 1px">
+                                <div class="ret">Retrieving..</div>
+                            </ul>
+                        </div>
+
+
+                    <!-- client's snapshot -->
                     <h2 class="title"><i class="glyphicon glyphicon-tag"></i>  CLIENT SNAPSHOT</h2>
                     <div class="form-group">
                         <table class="table table-striped op-table E1">
@@ -62,61 +55,55 @@ $err = '&nbsp;&nbsp;<div class="uk-badge uk-badge-danger" data-uk-tooltip="{pos:
                             </tbody>
                         </table>
                     </div>
+                </div>
 
+                <div id="get_los_laf_repeat_display">
                     <!-- repeat loan details  -->
-                    <?php if($cl_repeat['AccountID'] != NULL) { ?>
                         <h2 class="title"><i class="glyphicon glyphicon-tag"></i>  PREVIOUS ACCOUNT DETAILS</h2>
                         <div class="form-group">
                             <div class="row">
                                 <div class="col-md-4 text-bold">Account ID</div>
                                 <div class="col-md-8">
-                                    <span><?=$cl_repeat['AccountID']?></span>
+                                    <span><div class="ret">Retrieving..</div></span>
                                     <span class="font-weight-bold">Attendance Ratio</span>
                                     <span>
-                                    <?php
-                                    if($cl_repeat['MeetingCnt'] != 0 ) {
-                                        echo ($cl_repeat['AttendanceCnt'] / $cl_repeat['MeetingCnt']) * 100;
-                                    } else {
-                                        echo 0;
-                                    }
-
-                                    ?>%
+                                   <div class="ret">Retrieving..</div>
                                     </span>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-4 text-bold">No. of Arrear Days</div>
                                 <div class="col-md-8">
-                                    <span><?=$cl_repeat['NoOfArrearDays']?></span>
+                                    <span><div class="ret">Retrieving..</div></span>
                                     <span class="font-weight-bold">Attendance Count</span>
-                                    <span><?=$cl_repeat['AttendanceCnt']?></span>
+                                    <span><div class="ret">Retrieving..</div></span>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-4 text-bold">Delayed Payments</div>
                                 <div class="col-md-8">
-                                    <span><?=$cl_repeat['DelayedPymnt']?></span>
+                                    <span><div class="ret">Retrieving..</div></span>
                                     <span class="font-weight-bold">Meetings</span>
-                                    <span><?=$cl_repeat['MeetingCnt']?></span>
+                                    <span><div class="ret">Retrieving..</div></span>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-4 text-bold">Past Due Amount</div>
-                                <div class="col-md-8"><span>&#8369; <?=number_format($cl_repeat['PrinArrearAmount'], 2, '.', ',')?></span>
+                                <div class="col-md-8"><span>&#8369; <div class="ret">Retrieving..</div></span>
                                     <span class="font-weight-bold">Outstanding Principal</span>
-                                    <span>&#8369; <?=number_format($cl_repeat['OutstandingPrincipal'], 2, '.', ',')?></span>
+                                    <span>&#8369; <div class="ret">Retrieving..</div></span>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-4 text-bold">Maturity Date</div>
                                 <div class="col-md-8">
-                                    <span><?=$cl_repeat['MaturityDate']?></span>
+                                    <span><div class="ret">Retrieving..</div></span>
                                     <span class="font-weight-bold">Closed Date</span>
-                                    <span><?=$cl_repeat['ClosedDate']?></span>
+                                    <span><div class="ret">Retrieving..</div></span>
                                 </div>
                             </div>
                         </div>
-                    <?php } ?>
+                </div>
 
                     <!-- personal details -->
                     <h2 class="title"><i class="glyphicon glyphicon-tag"></i>  PERSONAL DETAILS</h2>
@@ -479,4 +466,16 @@ $err = '&nbsp;&nbsp;<div class="uk-badge uk-badge-danger" data-uk-tooltip="{pos:
         </div>
     </div>
 <?php $this->load->view("onepuhunan/copyright"); ?>
+
+ <script>
+    $(document).ready(function(){
+        $.get( "<?php echo base_url(); ?>/get_los_laf_repeat_display/<?php echo $input; ?>", function( data ) {
+            $( "#get_los_laf_repeat_display" ).html( data );
+        });
+
+        $.get( "<?php echo base_url(); ?>/get_los_laf_err/<?php echo $input; ?>", function( data ) {
+            $( "#get_los_laf_err" ).html( data );
+        });
+    });
+</script>
 <?php $this->load->view("onepuhunan/footer"); ?>
