705f2f392cc528cbe6385933afe93d53578f0d41
ajei.najera <ajei.najera@onepuhunan.com.ph>
2017-08-24 09:19:59 +0800

    ui for success page

--- application/views/validation/success_password.php
+++ application/views/validation/success_password.php
@@ -1,28 +1,25 @@
 <?php
 $data['title'] = 'OnePuhunan Service Portal | Registration Successful';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<?php $this->load->view("templates/head", $data); ?>
-<body>
-<?php $this->load->view("templates/header"); ?>
-<div class="page-wrap">
-    <div id="tm-container" class="uk-container uk-width-5-10 uk-container-center">
-        <?=form_open("", array("class" => "uk-form uk-form-horizontal"));?>
-        <legend class="uk-text-muted uk-text-bold uk-text-success tm-form-legend">
-            <i class="uk-icon-check-circle uk-text-success"></i> CHANGE TO DEFAULT PASSWORD SUCCESSFUL!
-        </legend>
-        <div class="uk-form-row tm-label tm-form-success">
-            <label class="uk-text-small">
-                You have successfully reset to default password. You can now login as usual with your Employee ID and the default password.
-            </label>
-        </div>
-        <div class="uk-form-row uk-text-center uk-margin-large-top">
-            <a class="uk-button uk-button-primary uk-button-small uk-width-3-10" href="<?php echo site_url("index.php"); ?>">GO TO DASHBOARD PAGE</a>
+<?php $this->load->view("onepuhunan/header",$data); ?>
+<div class="main">
+    <div class="main-inner">
+        <div class="container">
+        <?=form_open("", array("class" => "form-width-small text-center"));?>
+            <h3 class="text-success">
+                <i class=" glyphicon glyphicon-ok "></i> CHANGE TO DEFAULT PASSWORD SUCCESSFUL!
+            </h3>
+            <div class="form-group">
+                <label>
+                    You have successfully reset to default password. You can now login as usual with your Employee ID and the default password.
+                </label>
+            </div>
+            <div class="form-group">
+                <a class="btn global-button" href="<?php echo site_url("index.php"); ?>">GO TO DASHBOARD PAGE</a>
         </div>
         <?=form_close();?>
+        </div>
     </div>
 </div>
-<?php $this->load->view("templates/footer"); ?>
-</body>
-</html>
+<?php $this->load->view("onepuhunan/copyright"); ?>
+<?php $this->load->view("onepuhunan/footer"); ?>
