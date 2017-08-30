5f711c65ecf60129ec3f3fba73077f27c4fbd53c
ajei.najera <ajei.najera@onepuhunan.com.ph>
2017-08-30 14:12:43 +0800

    redirect to homepage when not logged in or when session expires

--- application/controllers/Audit.php
+++ application/controllers/Audit.php
@@ -6,6 +6,14 @@
 class Audit extends CI_Controller {
     public function __construct() {
         parent:: __construct();
+
+        $this->load->library('session');
+        if(!$this->session->userdata('logged_in')){
+            if(current_url() != base_url()){
+                redirect(base_url());
+
+            }
+        }
     }
 
     public function index() {
--- application/controllers/Hr.php
+++ application/controllers/Hr.php
@@ -3,6 +3,14 @@
     class Hr extends CI_Controller {
         public function __construct() {
             parent:: __construct();
+
+            $this->load->library('session');
+            if(!$this->session->userdata('logged_in')){
+                if(current_url() != base_url()){
+                    redirect(base_url());
+
+                }
+            }
         }
         
         public function index() {
